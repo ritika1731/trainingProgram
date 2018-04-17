2c3e327324919047891a9abc14d14684dc601bed
trainee <trainee@DIN16005416.corp.capgemini.com>
2018-04-17 12:23:10 +0530

    datetime

--- src/com/training/day1/DateTime.java
+++ src/com/training/day1/DateTime.java
@@ -8,29 +8,41 @@ import com.sun.org.apache.xerces.internal.impl.xpath.regex.ParseException;
 
 public class DateTime {
 	public static void main(String args[]) throws java.text.ParseException {
-		Scanner sc = new Scanner(System.in);
+		/*Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the first Date and time dd-MMM-yyyy HH:mm:ss: ");
 		String date = sc.next();
 		System.out.println("Enter the Second Date and time dd-MMM-yyyy HH:mm:ss: ");
-		String date1=sc.next();
+		String date1=sc.next();*/
+		
+		String date="31-JAN-2018 08:18:18";
+		String date1="01-FEB-2018 09:18:18";
 
 		SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss");
 		Date date2=null;
 		Date date3=null;
-		long difference=0;
+		//long difference=0;
 		try {
 		    //Parsing the String
 		    date2 = dateFormat.parse(date);
 		    date3 = dateFormat.parse(date1);
-		    difference=date3.getTime()-date2.getTime();
 		    
+		    long difference=date3.getTime()-date2.getTime();
+		    long diffSeconds = difference / 1000 % 60;
+			long diffMinutes = difference / (60 * 1000) % 60;
+			long diffHours = difference / (60 * 60 * 1000) % 24;
+			long diffDays = difference / (24 * 60 * 60 * 1000);
+			System.out.println(date2);
+			System.out.println(date3);
+			System.out.print(diffDays + " days, ");
+			System.out.print(diffHours + " hours, ");
+			System.out.print(diffMinutes + " minutes, ");
+			System.out.print(diffSeconds + " seconds.");
 		} catch (ParseException e) {
 		    // TODO Auto-generated catch block
 		    e.printStackTrace();
 		}
-		System.out.println(date2);
-		System.out.println(date3);
-		System.out.println(difference);
+		
+		
 		
 	}
 }
