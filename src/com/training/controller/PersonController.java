d87f205b4280b5439288d2680f5a3450a98c54dc
trainee <trainee@DIN16005416.corp.capgemini.com>
2018-04-18 13:59:24 +0530

    adding programs

--- src/com/training/controller/PersonController.java
+++ src/com/training/controller/PersonController.java
@@ -8,37 +8,25 @@ import com.training.bean.PhoneBean;
 public class PersonController {
 
 
-//	static void view()
-//	{
-//		System.out.println("Enter The Choice");
-//		System.out.println("============================");
-//		System.out.println("1  Add  Person");
-//		System.out.println("2  View all Person");
-//		System.out.println("3  Search by MobileNumber");
-//		System.out.println("4  Delete Person");
-//		System.out.println("5  Exit");
-//		System.out.println("============================");
-//	}
 
 	public static void main(String args[])
 	{
 		ArrayList<PhoneBean> phone  = new ArrayList<PhoneBean>();	
-
-		Scanner sc=new Scanner(System.in);
 
 		while(true)
 		{
 
 			System.out.println("###################################");
 			System.out.println("1. Add a Person.");
-			System.out.println("2. View details of given Person.");
+			System.out.println("2. View details of All Person.");
 			System.out.println("3. Search a Person.");
 			System.out.println("4. Remove a Person.");
 			System.out.println("5. Exit");
 			System.out.println("###################################");
 			System.out.println("Enter a choice please: ");
 			System.out.println("###################################");
-			//view();
+			
+			Scanner sc=new Scanner(System.in);
 			int choice =sc.nextInt();
 			if(choice<1 || choice >5)
 			{
@@ -51,6 +39,10 @@ public class PersonController {
 			{
 			case 1:
 				PhoneBean phonebook=new PhoneBean();
+				System.out.println("Enter Id");
+				String id=sc.next();
+				phonebook.setPersonId(id);
+
 				System.out.println("Enter FirstName");
 				String firstName=sc.next();
 				phonebook.setFirstName(firstName);
@@ -60,17 +52,16 @@ public class PersonController {
 				System.out.println("Enter Mobile Number ");
 				String mobileNumber=sc.next();
 				phonebook.setMobileNumber(mobileNumber);
-				System.out.println("Details Added Succesfully:" + phonebook.getFirstName()+" "+phonebook.getLastName()+" "+" has the number "+phonebook.getMobileNumber());
+				System.out.println("Details Added Succesfully");
 				phone.add(phonebook);
-
-				System.out.println(phone.size());
 				break;
 
 			case 2:
+				System.out.println(" View details of All Person.");
 
 				for(PhoneBean bean:phone)
 				{
-					System.out.println(bean);
+					System.out.println("Id"+ " "+bean.getPersonId());
 					System.out.println("First Name"+ " "+bean.getFirstName());
 					System.out.println("Last Name"+" " +bean.getLastName());
 					System.out.println("Mobile Number"+" "+bean.getMobileNumber());
@@ -79,16 +70,14 @@ public class PersonController {
 				break;
 			case 3:
 				System.out.println("Enter Mobile  Number to search:");
-				PhoneBean phonebook1=new PhoneBean();
 				String search=sc.next();
 				int count=0;
-
-
 
 				for(PhoneBean bean:phone)
 				{
 					if(bean.getMobileNumber().equals(search))
-					{count=1;
+					{
+						count=1;
 						
 					}
 				}
@@ -107,15 +96,30 @@ public class PersonController {
 				
 			case 4:
 				
-				
-				break;
+				System.out.println("Enter Mobile  Number to remove:");
+				String search1=sc.next();
+				int count1=-1;
+
+				for(PhoneBean bean:phone)
+				{
+					if(bean.getMobileNumber().equalsIgnoreCase(search1))
+					{
+						
+						count1=phone.indexOf(bean);
+						
+					}
+		
+
+				}
+				phone.remove(count1);
+		break;
 				
 			case 5:
 				System.exit(0);
+				System.out.println("Thank You!!");
 				break;
 			}
 		}
-			System.out.println("Thank You!!");
 	}
 
 	}
