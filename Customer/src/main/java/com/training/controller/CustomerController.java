5bc1b54c3f0f360b72b9539c20d4573495280ab9
trainee <trainee@DIN16005416.corp.capgemini.com>
2018-04-23 20:16:43 +0530

    spring

---
+++ Customer/src/main/java/com/training/controller/CustomerController.java
@@ -0,0 +1,21 @@
+package com.training.controller;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import com.training.entity.Customer;
+
+public class CustomerController {
+	
+	
+	public static void main(String[] args) {
+	
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		      
+		      Customer cust = (Customer) context.getBean("entity");
+		      Customer cust1= (Customer) context.getBean("entity2");
+		      
+		      System.out.println(cust.getFirstName());
+		      System.out.println(cust.getLastName());
+	}
+	
+}
---
+++ Customer/src/main/java/com/training/entity/Customer.java
@@ -0,0 +1,34 @@
+package com.training.entity;
+
+public class Customer {
+
+	
+	private String firstName;
+	private String lastName;
+	
+	
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+	/**
+	 * @param lastName the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+}
