6c35a4b3d2101bf223e2e9ab33d5e2afd806d6c7
ritika1731 <38352417+ritika1731@users.noreply.github.com>
2018-04-24 09:19:47 +0530

    Update FlexibleSavingAccount.java

--- Banking/src/com/traning/entity/FlexibleSavingAccount.java
+++ Banking/src/com/traning/entity/FlexibleSavingAccount.java
@@ -2,13 +2,26 @@ package com.traning.entity;
 
 public class FlexibleSavingAccount implements Account {
 
-	Customer customerobject=new Customer();
-	float getbalance=customerobject.getInitial_Amount();
-	@Override
-	public void calculateInterest() {
+	private Money money;
+	private InterestRate interestRate;
+	private Period interestPeriod;
 
-		float interest=(getbalance*6*1)/100;
-		System.out.println("Calculated Interest Is :"+interest);
+	public int deposit(int depositAmmount)
+	{
+		
+		return money.getMoney()+depositAmmount;
+	}
+	public int withdrawl(int withdrawlAmmount)
+	{
+		return money.getMoney();
+	}
+	
+	public int getBalance()
+	{
+		Scanner sc=new Scanner(System.in);
+		int interest=sc.nextInt();
+		interestRate.setInterestRate(interest);
+		return interestRate.getInterestRate()*money.getMoney()*interestPeriod.getPeriod()/100;
 	}
 	
 	
