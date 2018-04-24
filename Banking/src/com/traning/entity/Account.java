26060ec932d51e45074e8ea2b3d9c6124fc75ae4
ritika1731 <38352417+ritika1731@users.noreply.github.com>
2018-04-24 09:22:20 +0530

    Update Account.java

--- Banking/src/com/traning/entity/Account.java
+++ Banking/src/com/traning/entity/Account.java
@@ -2,5 +2,8 @@ package com.traning.entity;
 
 interface  Account {
 
-	public void calculateInterest();
+	public void getBalance();
+	public int deposit(int depositAmmount);
+	public int withdraw(int withdrawAmmount);
+
 }
